3e7f1a54fd9b2bfe3f6fcd7314d02f8f47d33afa
flips30240 <flips92691@gmail.com>
2016-10-06 03:54:42 -0700

    started world geom generation
    lots to do still

--- source/createLight.py
+++ source/createLight.py
@@ -5,20 +5,26 @@ class createLight():
 
         if str == "world":
             self.add_world_spotlight(nodepath)
+            self.add_world_ambient_light()
         else:
             print("keyword " + str + " is not recognized!")
 
     def add_world_spotlight(self, nodeToFollow):
-        self.worldSLight = base.render.attachNewNode(DirectionalLight("world spotlight"))
-        #self.worldSLight.node().setScene(base.render)
-        self.worldSLight.node().setShadowCaster(True, 1024, 1024)
-        self.worldSLight.node().showFrustum()
-        #self.worldSLight.node().getLens().setFov(30)
-        self.worldSLight.node().getLens().setNearFar(1, 400)
-        self.worldSLight.node().getLens().setFilmSize(50)
-        #self.worldSLight.reparentTo(base.render.find("player"))
-        self.worldSLight.setPos(50, 0, 50)
-        self.worldSLight.lookAt(nodeToFollow)
-        base.render.setLight(self.worldSLight)
+        self.worldLight = base.render.attachNewNode(DirectionalLight("world light"))
+        #self.worldLight.node().setScene(base.render)
+        self.worldLight.node().setShadowCaster(True, 512, 512)
+        self.worldLight.node().showFrustum()
+        self.worldLight.node().getLens().setFov(30)
+        self.worldLight.node().getLens().setNearFar(30, 150)
+        self.worldLight.node().getLens().setFilmSize(200)
+        #self.worldLight.reparentTo(base.render.find("player"))
+        self.worldLight.setPos(50, 0, 50)
+        self.worldLight.lookAt(nodeToFollow)
+        base.render.setLight(self.worldLight)
 
         base.render.setShaderAuto()
+
+    def add_world_ambient_light(self):
+        self.worldAmbLight = base.render.attachNewNode(AmbientLight("world ambient light"))
+        self.worldAmbLight.node().setColor(VBase4(.2, .2, .2, 1))
+        base.render.setLight(self.worldAmbLight)
--- source/world.py
+++ source/world.py
@@ -1,11 +1,56 @@
 from shapes.shapeGenerator import Cube, Sphere
+import random
+import math
 
 class world():
 
     def __init__(self):
         self.create_dev_world(100, 100, 1)
+        self.create_test_island()
 
     def create_dev_world(self, l, w, h):
         self.devWorldGeom = Cube(l, w, h)
         self.devWorldNode = base.render.attachNewNode("world")
         self.devWorldGeom.reparentTo(self.devWorldNode)
+
+    def create_test_island(self):
+        def determine_location():
+            self.testIslandNode = base.render.attachNewNode("test island")
+            self.testIslandNode.setPos(-50, 120, 0)
+        def determine_bounds(min, max):
+            self.bounds = random.randint(min, max)
+        def determine_dimensions():
+            self.width = self.bounds
+            self.length = (self.width/random.randint(1, 6))
+            self.length = int(((math.ceil(self.length/2))*2) + 1)
+            if self.width >= self.length:
+                self.height = self.length
+            else:
+                self.height = self.width
+            print(self.width, self.length, self.height)
+        def create_top():
+            self.topGeom = Cube(self.width, self.length, 1)
+            self.topGeom.setPos(0, 0, 0)
+            self.topGeom.reparentTo(self.testIslandNode)
+        def create_bottom():
+            self.posList = []
+            for z in range(self.height):
+                for x in range(self.width):
+                    for y in range(self.length):
+                        if x == self.width - z:
+                            self.posList.append([x, y, z])
+                        if y == self.length - z:
+                            self.posList.append([x, y, z])
+
+            for x in range(len(self.posList)):
+                testCube = Cube(1, 1, 1)
+                testCube.setPos(self.posList[x][0], self.posList[x][1], self.posList[x][2])
+                testCube.reparentTo(self.testIslandNode)
+                print(self.posList[x])
+            #self.testIslandNode.flattenStrong()
+
+        determine_location()
+        determine_bounds(10, 100)
+        determine_dimensions()
+        create_top()
+        create_bottom()
